47bc79c06d782c6ad629be9a7295041af8011336
a <jazulado1337@hotmail.com>
2019-05-29 20:24:11 -0500

    index

---
+++ application/views/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+			<?php include("includes/includecss.php"); ?>
+</head>
+<body><div></div>
+			<?php include("includes/menu.php"); ?>
+	
+
+    <div id="right-panel" class="right-panel">
+			<?php include("includes/header.php"); ?>
+			<?php include("includes/breadcumb.php"); ?>
+			<?php include("includes/contenidoinicio.php"); ?>
+		</div>
+
+			<?php include("includes/includejs.php"); ?>
+</body>
+</html>
+
+
+
